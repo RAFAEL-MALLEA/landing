de4ab9aa60c92befdc4c67a3ba6dd8316c615b8f
RAFAEL-MALLEA <raf.mallea@duocuc.cl>
2025-01-20 12:06:48 -0300

    guardar cambio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Esto ignora los errores de ESLint durante la construcción
+  },
 };
-
 export default nextConfig;
